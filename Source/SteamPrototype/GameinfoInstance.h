92c9aeeb3923f72c4025a69cc7bcfceb90192ba7
Hoang Gia Ninh <ninhhoangDev@gmail.com>
2020-02-16 12:47:57 +1300

    Create asset for UI and MAP of Main Menu, Loading Map, Lobby, 2 test Maps, Player UI, Player Chat.

---
+++ Source/SteamPrototype/GameinfoInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "GameinfoInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STEAMPROTOTYPE_API UGameinfoInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/SteamPrototype/SteamPrototypeGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SteamPrototypeGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASteamPrototypeGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASteamPrototypeGameMode();
+};
+
+
+
