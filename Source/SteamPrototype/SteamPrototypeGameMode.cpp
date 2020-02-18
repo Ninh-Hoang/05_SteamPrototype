47f59ec52f8bee88553e8d93eba918310bf41998
Hoang Gia Ninh <ninhhoangDev@gmail.com>
2020-02-18 16:38:38 +1300

    Setup Lobby UI.

--- Source/SteamPrototype/SteamPrototypeGameMode.cpp
+++ Source/SteamPrototype/SteamPrototypeGameMode.cpp
@@ -7,7 +7,7 @@
 ASteamPrototypeGameMode::ASteamPrototypeGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprints/Characters/BP_Base"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
