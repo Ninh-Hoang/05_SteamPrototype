4a0e0989d9b59bae482a98beaf0ea9b9dcf3b8ad
Hoang Gia Ninh <ninhhoangDev@gmail.com>
2020-02-21 01:02:53 +1300

    Setup Projectile BP

---
+++ Source/SteamPrototype/ProjectileMovement.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "ProjectileMovement.generated.h"
+
+/**
+ * 
+ */
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class STEAMPROTOTYPE_API UProjectileMovement : public UProjectileMovementComponent
+{
+	GENERATED_BODY()
+	
+};
